6f3c73a00a8a191676c2da8333833f6e9195ed30
Brad Crittenden <bac@canonical.com>
2014-10-06 09:48:02 +0200

    Update to use the new entity with camelcase support.

--- ElasticSearch/go.sh
+++ ElasticSearch/go.sh
@@ -5,13 +5,13 @@ set -eux
 # :9200 implies localhost:9200
 http DELETE :9200/charmstore/
 
-http PUT :9200/charmstore < index2.json
+http PUT :9200/charmstore < index.json
 
 http PUT :9200/charmstore/entity/_mapping < entity.json
 
 http GET :9200/charmstore/_settings
 
-http POST :9200/charmstore/entity/mw < mediawiki-charm.json
+http POST :9200/charmstore/entity/mw < legacy/mediawiki-charm.json
 
-# An example of ngram searching. Note the quotes are required.
-#http GET ":9200/charmstore/entity/_search" < search_ngram.json
+# An example of ngram searching.
+#http GET :9200/charmstore/entity/_search < search_ngram.json
